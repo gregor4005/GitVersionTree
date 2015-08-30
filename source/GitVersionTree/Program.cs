a87d0fad94419a9f745674e7a90df54faf8e3685
Günther Foidl <gue@korporal.at>
2015-08-30 21:13:57 +0200

    Basic command-line added.

--- source/GitVersionTree/Program.cs
+++ source/GitVersionTree/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Windows.Forms;
 
 namespace GitVersionTree
@@ -11,7 +13,7 @@ namespace GitVersionTree
 		/// The main entry point for the application.
 		/// </summary>
 		[STAThread]
-		static void Main()
+		static void Main(string[] args)
 		{
 			if (Properties.Settings.Default.NeedsUpgrade)
 			{
@@ -20,9 +22,31 @@ namespace GitVersionTree
 				Properties.Settings.Default.Save();
 			}
 
+			if (args.Length == 0)
+				RunAsWinform();
+			else
+				RunAsCommandLine(args);
+		}
+		//---------------------------------------------------------------------
+		private static void RunAsWinform()
+		{
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
 			Application.Run(new MainForm());
 		}
+		//---------------------------------------------------------------------
+		private static void RunAsCommandLine(string[] args)
+		{
+			AllocConsole();
+
+			Console.WriteLine("\nBye.");
+
+			if (Debugger.IsAttached)
+				Console.ReadKey();
+		}
+		//---------------------------------------------------------------------
+		[DllImport("kernel32.dll", SetLastError = true)]
+		[return: MarshalAs(UnmanagedType.Bool)]
+		static extern bool AllocConsole();
 	}
 }
